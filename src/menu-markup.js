47b4f220a780772e320a6562874818e82c6fc0b6
Zhivotenko Oleksandr <zhivotenko.a.s@gmail.com>
2020-10-07 13:37:31 +0300

    Update menu-markup.js

--- src/menu-markup.js
+++ src/menu-markup.js
@@ -3,10 +3,6 @@ import foodServiceTemplate from './food-service.hbs';
 
 const menuGallery = document.querySelector('ul.js-menu');
 
-// function createFoodServiceMarkup(foodCards) {
-//     return foodServiceTemplate(foodCards);
-// }
-
 const cardsMarkup = foodServiceTemplate(foodCards);
 
 menuGallery.insertAdjacentHTML('beforeend', cardsMarkup);
