6b0adc01623ad7ea1e60ca9b650409c6d770185d
Zhivotenko Oleksandr <zhivotenko.a.s@gmail.com>
2020-10-07 13:55:35 +0300

    Update toggle-theme.js

--- src/toggle-theme.js
+++ src/toggle-theme.js
@@ -1,19 +1,25 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
 const toggle = document.querySelector('#theme-switch-toggle');
 const body = document.querySelector('#body');
 
 toggle.addEventListener('change', onToggleChangeTheme);
 
 currentTheme();
-body.classList.add('light-theme');
+body.classList.add(Theme.LIGHT);
 
 function onToggleChangeTheme() {
     if (toggle.checked === true) {
-        themeToggle = body.classList.add('dark-theme');
-        body.classList.remove('light-theme');
+        themeToggle = body.classList.add(Theme.DARK);
+        body.classList.remove(Theme.LIGHT);
         localStorage.setItem('siteTheme', true);
     } else {
-        themeToggle = body.classList.add('light-theme');
-        body.classList.remove('dark-theme');
+        themeToggle = body.classList.add(Theme.LIGHT);
+        body.classList.remove(Theme.DARK);
         localStorage.setItem('siteTheme', false);
     }
 }
@@ -22,7 +28,7 @@ function currentTheme() {
     const themeStorage = localStorage.getItem('siteTheme');
     if (themeStorage) {
         if (themeStorage === 'true') {
-            body.classList.add('dark-theme');
+            body.classList.add(Theme.DARK);
             toggle.checked = true;
         }
     }
